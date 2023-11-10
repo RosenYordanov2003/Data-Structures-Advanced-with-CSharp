662bf9c7f71f97315e71ea589b5ecf371c8c0a24
Rosen Yordanov <roskata182003@gmail.com>
2023-11-10 19:35:55 +0200

    Upload done B-Trees 2-3 Insertion

---
+++ 01. B-Trees, 2-3-Trees Lab/Implement 2-3 Tree Insertion/01. B-Trees-2-3-Trees Lab-Skeleton/01.Two-Three/TwoThreeTree.cs
@@ -0,0 +1,149 @@
+﻿namespace _01.Two_Three
+{
+    using System;
+    using System.Text;
+
+    public class TwoThreeTree<T> where T : IComparable<T>
+    {
+        private TreeNode<T> root;
+
+        public void Insert(T element)
+        {
+            root = this.Insert(root, element);
+        }
+
+        private TreeNode<T> Insert(TreeNode<T> node, T element)
+        {
+            if (node == null)
+            {
+                return new TreeNode<T>(element);
+            }
+            else if (node.IsLeaf())
+            {
+                return MergeNodes(node, new TreeNode<T>(element));
+            }
+            else if (node.LeftKey.CompareTo(element) > 0)
+            {
+                var newNode = Insert(node.LeftChild, element);
+
+                return newNode == node.LeftChild ? node : MergeNodes(node, newNode);
+            }
+            else if (node.RightKey!= null && node.RightKey.CompareTo(element) < 0)
+            {
+                var newNode = Insert(node.RightChild, element);
+                return newNode == node.RightChild ? node : MergeNodes(node, newNode);
+            }
+            else
+            {
+                var newNode = Insert(node.MiddleChild, element);
+                return newNode == node.MiddleChild ? node : MergeNodes(node, newNode);
+            }
+        }
+
+        private TreeNode<T> MergeNodes(TreeNode<T> currentNode, TreeNode<T> newNode)
+        {
+            if (currentNode.IsTwoNode())
+            {
+                //When current left key is bigger than new key it moves to the right
+                if (currentNode.LeftKey.CompareTo(newNode.LeftKey) > 0)
+                {
+                    currentNode.RightKey = currentNode.LeftKey;
+                    currentNode.LeftKey = newNode.LeftKey;
+
+                    currentNode.RightChild = currentNode.MiddleChild;
+                    currentNode.MiddleChild = newNode.MiddleChild;
+                    currentNode.LeftChild = newNode.LeftChild;
+                }
+                else
+                {
+                    currentNode.RightKey = newNode.LeftKey;
+                    currentNode.MiddleChild = newNode.LeftChild;
+                    currentNode.RightChild = newNode.MiddleChild;
+                }
+                return currentNode;
+            }
+            else if (newNode.LeftKey.CompareTo(currentNode.LeftKey) < 0)
+            {
+                var node = new TreeNode<T>(root.LeftKey)
+                {
+                    LeftChild = newNode,
+                    MiddleChild = currentNode,
+                };
+                currentNode.LeftChild = currentNode.MiddleChild;
+                currentNode.MiddleChild = currentNode.RightChild;
+                currentNode.LeftKey = currentNode.RightKey;
+                currentNode.RightKey = default;
+                currentNode.RightChild = null;
+
+                return node;
+            }
+            else if (newNode.LeftKey.CompareTo(currentNode.RightKey) < 1)
+            {
+                newNode.MiddleChild = new TreeNode<T>(currentNode.RightKey)
+                {
+                    LeftChild = newNode.MiddleChild,
+                    MiddleChild = currentNode.RightChild
+                };
+                newNode.LeftChild = currentNode;
+                currentNode.RightKey = default;
+                currentNode.RightChild = null;
+
+                return newNode;
+            }
+            else
+            {
+                var node = new TreeNode<T>(currentNode.RightKey)
+                {
+                    LeftChild = currentNode,
+                    MiddleChild = newNode,
+                };
+                newNode.LeftChild = currentNode.RightChild;
+                currentNode.RightKey = default;
+                currentNode.RightChild = null;
+
+                return node;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            RecursivePrint(this.root, sb);
+            return sb.ToString();
+        }
+
+        private void RecursivePrint(TreeNode<T> node, StringBuilder sb)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            if (node.LeftKey != null)
+            {
+                sb.Append(node.LeftKey).Append(" ");
+            }
+
+            if (node.RightKey != null)
+            {
+                sb.Append(node.RightKey).Append(Environment.NewLine);
+            }
+            else
+            {
+                sb.Append(Environment.NewLine);
+            }
+
+            if (node.IsTwoNode())
+            {
+                RecursivePrint(node.LeftChild, sb);
+                RecursivePrint(node.MiddleChild, sb);
+            }
+            else if (node.IsThreeNode())
+            {
+                RecursivePrint(node.LeftChild, sb);
+                RecursivePrint(node.MiddleChild, sb);
+                RecursivePrint(node.RightChild, sb);
+            }
+        }
+    }
+}
---
+++ 01. B-Trees, 2-3-Trees Lab/Implement 2-3 Tree Insertion/01. B-Trees-2-3-Trees Lab-Skeleton/Demo/Program.cs
@@ -0,0 +1,52 @@
+﻿using _01.Two_Three;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main()
+        {
+            var tree = new TwoThreeTree<string>();
+
+            string[] arr = { "F", "C", "G", "A", "B", "D", "E", "K", "I", "G", "H", "J", "K" };
+            for (int i = 0; i < 13; i++)
+            {
+                tree.Insert(arr[i]);
+            }
+            Console.WriteLine(tree.ToString());
+        }
+
+
+       private class IntWrapper : IComparable<IntWrapper>
+       {
+            public IntWrapper(int value)
+            {
+                Value = value;
+            }
+            public int Value { get; set; }
+
+
+            public int CompareTo([AllowNull] IntWrapper other)
+            {
+                if (this.Value > other.Value)
+                {
+                    return 1;
+                }
+                else if (this.Value == other.Value)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            public override string ToString()
+            {
+                return this.Value.ToString();
+            }
+       }
+    }
+}
